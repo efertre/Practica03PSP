551edc79b83c6963b86e2953dc439cd28d5817db
efertre <efertre8@gmail.com>
2024-11-15 18:53:20 +0100

    Interfaz fecha calculable

--- 03 PRACTICA/src/model/FechaCalculable.java
+++ 03 PRACTICA/src/model/FechaCalculable.java
@@ -1,5 +1,25 @@
 package model;
 
+import java.util.Calendar;
+
+/**
+ * (Modelo) Desarrolla un interfaz a implementar sobre las clase hijas para los
+ * cálculo con fechas. Deberá: 
+ * 2.1.- Tener los métodos para controlar si se
+ * cumplen meses o años. Dichos métodos no tendrán parámetros y compararán la
+ * fecha de apertura de la cuenta y la fecha del sistema. 
+ * 
+ * 2.2.- Tendrá las
+ * constantes de la clase Calendar (DAY_OF_MONTH, MONTH y YEAR) en español.
+ */
+
 public interface FechaCalculable {
+
+	final int DIA_DEL_MES = Calendar.DAY_OF_MONTH;
+	final int MES = Calendar.MONTH;
+	final int ANYO = Calendar.YEAR;
+	
+	public void cumplirMes();
+	public void cumplirAnyo();
 
 }

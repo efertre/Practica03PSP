ee2b2f1da67c7ef181dc9f39c39defd6ed8c27d9
Paula Sanchez Velez <psanchezvelez03@gmail.com>
2024-11-22 15:36:56 +0100

    Cambio interfaz al padre

--- 03 PRACTICA/src/model/CuentaCorriente.java
+++ 03 PRACTICA/src/model/CuentaCorriente.java
@@ -33,31 +33,4 @@ public class CuentaCorriente extends Cuenta implements FechaCalculable {
 		this.tipo = tipo;
 	}
 	
-	
-
-	@Override
-	public int cumplirMes() {
-		
-		
-		int dias = ((FechaCalculable.ANYO - super.getFechaApertura().getYear()) * 365 +
-				(FechaCalculable.MES - super.getFechaApertura().getMonthValue()) * 30 +
-				(FechaCalculable.DIA_DEL_MES - super.getFechaApertura().getDayOfMonth()));
-		
-		
-		return dias / 30;
-	}
-
-
-	@Override
-	public int cumplirAnyo() {
-		int dias = ((FechaCalculable.ANYO - super.getFechaApertura().getYear()) * 365 +
-				(FechaCalculable.MES - super.getFechaApertura().getMonthValue()) * 30 +
-				(FechaCalculable.DIA_DEL_MES - super.getFechaApertura().getDayOfMonth()));
-		
-		
-		return dias / 365;
-	}
-
-
-	
 }

132d84e46019211f64d12a1367e07ff8d24ed831
efertre <efertre8@gmail.com>
2024-11-29 15:57:54 +0100

    Lectura y escritura de archivo binario (aún queda verificar si está vacío y algunos errores)

--- 03 PRACTICA/src/model/Cuenta.java
+++ 03 PRACTICA/src/model/Cuenta.java
@@ -1,23 +1,29 @@
 package model;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.time.LocalDate;
 
 import model.exceptions.ESaldoNoValido;
 
-public class Cuenta implements FechaCalculable {
-
+public class Cuenta implements FechaCalculable, Serializable {
+	private static final long serialVersionUID = 1L;
 	private Integer numero;
 	private transient String titular;
 	private Double saldo;
 	private Double saldoMinimo;
 	private LocalDate fechaApertura;
 
-	public Cuenta(Integer numero, String titular, Double saldo, Double saldoMinimo, LocalDate fechaApertura) throws ESaldoNoValido {
+	public Cuenta(Integer numero, String titular, Double saldo, Double saldoMinimo, LocalDate fechaApertura)
+			throws ESaldoNoValido {
 		super();
 		setNumero(numero);
 		setTitular(titular);
-		setSaldo(saldo);
 		setSaldoMinimo(saldoMinimo);
+		setSaldo(saldo);
+		
 		setFechaApertura(fechaApertura);
 
 	}
@@ -44,16 +50,19 @@ public class Cuenta implements FechaCalculable {
 	}
 
 	public void setSaldo(Double saldo) throws ESaldoNoValido {
-		if (saldo < saldoMinimo)
-			throw new ESaldoNoValido("Saldo no válido [SALDO INFERIOR QUE EL SALDO MÍNIMO]");
-		this.saldo = saldo;
+		if (saldo != null) {
+			if (saldo < saldoMinimo)
+				throw new ESaldoNoValido("Saldo no válido [SALDO INFERIOR QUE EL SALDO MÍNIMO]");
+
+			this.saldo = saldo;
+		}
 	}
 
 	public Double getSaldoMinimo() {
 		return saldoMinimo;
 	}
 
-	public void setSaldoMinimo(Double saldoMinimo) {
+	public void setSaldoMinimo(double saldoMinimo) {
 		this.saldoMinimo = saldoMinimo;
 	}
 
@@ -68,22 +77,38 @@ public class Cuenta implements FechaCalculable {
 	@Override
 	public int cumplirMes() {
 
-		int dias = ((FechaCalculable.ANYO - getFechaApertura().getYear()) * 365 +
-				(FechaCalculable.MES - getFechaApertura().getMonthValue()) * 30 +
-				(FechaCalculable.DIA_DEL_MES - getFechaApertura().getDayOfMonth()));
-		
-		
+		int dias = ((FechaCalculable.ANYO - getFechaApertura().getYear()) * 365
+				+ (FechaCalculable.MES - getFechaApertura().getMonthValue()) * 30
+				+ (FechaCalculable.DIA_DEL_MES - getFechaApertura().getDayOfMonth()));
+
 		return dias / 30;
 	}
 
 	@Override
 	public int cumplirAnyo() {
-		int dias = ((FechaCalculable.ANYO - getFechaApertura().getYear()) * 365 +
-				(FechaCalculable.MES - getFechaApertura().getMonthValue()) * 30 +
-				(FechaCalculable.DIA_DEL_MES - getFechaApertura().getDayOfMonth()));
-		
-		
+		int dias = ((FechaCalculable.ANYO - getFechaApertura().getYear()) * 365
+				+ (FechaCalculable.MES - getFechaApertura().getMonthValue()) * 30
+				+ (FechaCalculable.DIA_DEL_MES - getFechaApertura().getDayOfMonth()));
+
 		return dias / 365;
 	}
+	
+	private void writeObject(ObjectOutputStream oos) throws IOException {
+	    oos.defaultWriteObject(); // Serializa los campos no transient
+	    oos.writeUTF(titular == null ? "" : titular); // Serializa manualmente `titular`
+	}
+
+	private void readObject(ObjectInputStream ois) throws IOException, ClassNotFoundException {
+	    ois.defaultReadObject(); // Deserializa los campos no transient
+	    this.titular = ois.readUTF(); // Deserializa manualmente `titular`
+	}
+
+	@Override
+	public String toString() {
+		return "Cuenta [numero=" + numero + ", titular=" + titular + ", saldo=" + saldo + ", saldoMinimo=" + saldoMinimo
+				+ ", fechaApertura=" + fechaApertura + "]";
+	}
+	
+	
 
 }
--- 03 PRACTICA/src/model/CuentaCorriente.java
+++ 03 PRACTICA/src/model/CuentaCorriente.java
@@ -1,11 +1,16 @@
 package model;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
 import model.exceptions.ESaldoNoValido;
 
-public class CuentaCorriente extends Cuenta implements FechaCalculable {
+public class CuentaCorriente extends Cuenta implements FechaCalculable, Serializable {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private Double comisionMantenimiento;
 	private TipoComision tipo;
 	
@@ -32,5 +37,13 @@ public class CuentaCorriente extends Cuenta implements FechaCalculable {
 	public void setTipo(TipoComision tipo) {
 		this.tipo = tipo;
 	}
+
+	@Override
+	public String toString() {
+		return "CuentaCorriente [comisionMantenimiento=" + comisionMantenimiento + ", tipo=" + tipo + ", toString()="
+				+ super.toString() + "]";
+	}
+	
+	
 	
 }
